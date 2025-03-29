2d312ddd449a5010a599be312645577862e352d7
Nilay Gaitonde <68230775+NilayGaitonde@users.noreply.github.com>
2025-03-29 15:00:19 +0530

    added time calculation

--- sink.py
+++ sink.py
@@ -1,7 +1,7 @@
 import zmq
 import time
 import sys
-
+import timeit
 
 def sink(num_workers):
     context = zmq.Context()
@@ -16,20 +16,25 @@ def sink(num_workers):
     for _ in range(num_workers + 1):
         worker_id = worker.recv_string()
         print(f"Received worker ID: {worker_id}")
-
+    start_time = timeit.default_timer()
     fact = 1
     try:
         for _ in range(num_workers):
+            print("Waiting for worker to complete")
             result = worker.recv_string()
             try:
                 fact*=int(result)
             except ValueError:
-                pass
+                break
     except KeyboardInterrupt:
+        pass
+    finally:
         worker.close()
+        completed.close()
         context.term()
+    elapsed = timeit.default_timer() - start_time
+    print(f"Elapsed time: {elapsed}")
     print(f"Factorial is {fact}")
-    completed.send_string("DONE")
 
 if __name__ == "__main__":
     sink(int(sys.argv[1]))
